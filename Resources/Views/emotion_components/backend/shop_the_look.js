8a8be78355cea78ef5b6b0065118db03d220cd97
hostianer <info@hostianer.hosting>
2017-05-10 09:50:32 +0200

    Removed Icon and initComponent

--- Resources/Views/emotion_components/backend/shop_the_look.js
+++ Resources/Views/emotion_components/backend/shop_the_look.js
@@ -3,14 +3,6 @@ Ext.define('Shopware.apps.Emotion.view.components.ShopTheLook', {
 
     extend: 'Shopware.apps.Emotion.view.components.Base',
 
-    alias: 'widget.emotion-components-shop-the-look',
-
-    icon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAgCAYAAAB6kdqOAAAABGdBTUEAALGPC/xhBQAAAZhJREFUWAntls8rRFEUx4dBIiIpUvIHsFFS/ghkY6dYyNZCWVgpZUf2lJRsZG1vRxQ2UnYWrORHCMPn+2p0ujNv7jOYmcU99ZnOveece8+cd87MS6f80onLKMzCGnTDPnRBH7zAO9RAG/SD9p5gHVahFxrhFrT/Y6knYgbO4NPhlXU7NMCVY5PvOdRCD3yAjc+wPoZJqAOvtOKxADdgD3J1VUqiau3BM+ib70AHSDbBjbPra+xz0Aw5ooNX4BFsUCF9Ht8qkFRDVk+jL0GhWGu7w3cZ1BqRKJE3sE5J9SPipmAABmEa8j3mJOepFRYhdQFJAkrhc6jyaiKGoRJEkxyNa75pKUVF7B2n5JLtw2gMrbEc+ph9RPpRu4RyJKI7T+C7OuiRTPBpE1LX3zt71l6srjF3p3qEvRxRg2/DBqh8TbALxV4cF6e/kxYYhy3QOrH8V0KxCegXtqIkJOR7HKFCoUK+CvjsoYdChXwV8NlDD4UK+Srgs4ce+m2F9E7911LwzJw3fed2vfsOgc/PCYtdZrAcwEOcxxcN+EHepB2XOwAAAABJRU5ErkJggg==',
-
-    initComponent: function () {
-        var me = this;
-
-        me.callParent(arguments);
-    }
+    alias: 'widget.emotion-components-shop-the-look'
 });
 //{/block}

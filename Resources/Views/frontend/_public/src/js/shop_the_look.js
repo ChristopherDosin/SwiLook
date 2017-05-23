9932cd2b0b1924bcbba0febcbb4866365a19e1d4
hostianer <info@hostianer.hosting>
2017-05-23 12:24:37 +0200

    updated

--- Resources/Views/frontend/_public/src/js/shop_the_look.js
+++ Resources/Views/frontend/_public/src/js/shop_the_look.js
@@ -1,6 +1,7 @@
 $.subscribe('plugin/swEmotionLoader/onLoadEmotionFinished', function() {
 
     $( ".shopthelook-thumbnail" ).unbind('click').click(function() {
+        //$.overlay.open();
         $(this).next().animate({width: 'toggle'});
     });
 
